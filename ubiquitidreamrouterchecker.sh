38de67c248ba3056a6ced1d45844a0ef789e6139
Frank Gao <gtx.seattle@gmail.com>
2022-09-30 15:56:53 -0700

    after send a SMS, make a call

--- ubiquitidreamrouterchecker.sh
+++ ubiquitidreamrouterchecker.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
+twilio_number="+11234567890"
+your_number="+11234567890"
+account_sid="XXX"
+auth_token="XXX"
+
 sendAlertSMS()
 {
-    account_sid="xxxxxx"
-    auth_token="xxxxxx"
-
-    twilio_number="+14256544791"
-    your_number="+15155986420"
-
     echo $(date -u) "Send SMS to $your_number"
-    curl -X POST -d "Body=Dream Router is in stock!" \
+    curl -X POST -d "Body=Dream Router is in stock! https://store.ui.com/collections/unifi-network-unifi-os-consoles/products/dream-router" \
         -d "From=${twilio_number}" -d "To=${your_number}" \
         "https://api.twilio.com/2010-04-01/Accounts/${account_sid}/Messages" \
         -u "${account_sid}:${auth_token}"
+}
+
+makeAlertCall()
+{
+    echo $(date -u) "Call $your_number"
+    curl -X POST https://api.twilio.com/2010-04-01/Accounts/${account_sid}/Calls.json \
+    --data-urlencode "Url=https://handler.twilio.com/twiml/EHcb8906a22908e88b559a0a2f01d896a6" \
+    --data-urlencode "To=${your_number}" \
+    --data-urlencode "From=${twilio_number}" \
+    -u ${account_sid}:${auth_token}
 }
 
 echo $(date -u) "Start to track"
@@ -22,6 +31,7 @@ do
     if curl -s 'https://store.ui.com/collections/unifi-network-unifi-os-consoles/products/dream-router' | grep -q 'In Stock</span>'; then
         echo $(date -u) "Yes, in stock!"
         sendAlertSMS
+        makeAlertCall
         echo $(date -u) "Check again in 3600 seconds = 60 mins"
         sleep 3600
     else
@@ -29,4 +39,3 @@ do
         sleep 60
     fi
 done
-
