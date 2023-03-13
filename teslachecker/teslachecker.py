41196a619eaf08e50e078171f22f381c7a8dd9c1
Frank Gao <gtx.seattle@gmail.com>
2023-03-13 01:12:15 -0700

    fix it

--- teslachecker/teslachecker.py
+++ teslachecker/teslachecker.py
@@ -1,6 +1,15 @@
 import requests
 import json
 import time
+import pytz
+from datetime import datetime
+
+def get_current_time_pst():
+    # Set timezone to Pacific Time
+    tz = pytz.timezone('America/Los_Angeles')
+    # Get current time in Pacific Time
+    current_time = datetime.now(tz)
+    return current_time.strftime('%Y-%m-%d %H:%M:%S %Z%z')
 
 def filter_item(d):
     return d.get("year") == 2023 and d.get("stateProvince") == "WA" and (d.get("trimCode") == "LRAWD" or d.get("trimCode") == "PAWD") and d.get("isAvailable") == True and d.get("isDemo") == False
@@ -26,12 +35,12 @@ def send_twilio_sms(message):
     print(response.text)
 
 def get_and_send_sms():
-    url = "https://api.waitingfortesla.com/api/v1/inventory?countryCode=US&vehicleModel=my&vehicleTitle=NEW"
+    url = 'https://api.waitingfortesla.com/api/v1/inventory?countryCode=US&vehicleModel=my&vehicleTitle=NEW'
     headers = {
         "accept": "*/*",
         "accept-encoding": "gzip, deflate, br",
         "accept-language": "en,zh-CN;q=0.9,zh;q=0.8,zh-TW;q=0.7",
-        "if-none-match": 'W/"33184-CfZKAuOLFC9T7NiJiyYJ9VTft5U"',
+        # "if-none-match": 'W/"2cd38-oG3umw9/7u8n8BHoJgWNyNWLd2U"',
         "origin": "https://waitingfortesla.com",
         "referer": "https://waitingfortesla.com/",
         "sec-ch-ua": '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
@@ -59,7 +68,7 @@ def get_and_send_sms():
         if len(output_data) > 0:
             send_twilio_sms(json.dumps(output_data, indent=4))
         else:
-            send_twilio_sms("No available model Y: https://www.tesla.com/inventory/new/my?arrangeby=relevance&zip=98021&range=200")
+            send_twilio_sms("No available model Y " + get_current_time_pst())
     else:
         print(f"Request failed with status code {response.status_code}")
 
